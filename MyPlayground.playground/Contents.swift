5040de889dd22dca3da132e00a7b52e8ad046003
Charles Wang <charles@stratim.com>
2020-02-14 16:01:12 -0800

    Depth vs Breadth

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -44,3 +44,137 @@ func bottomUp(_ place: Int) -> Int {
 }
 //bottomUp(9)
 
+class Node {
+    var value   : String
+    var nodes   = [Node]()
+    var visited = false
+
+    init(_ value: String) {
+        self.value = value
+    }
+    func add(_ nodeArray: [Node]) {
+        nodes = nodeArray
+    }
+}
+class BinaryNode {
+    var value       : String
+    var leftNode    : BinaryNode?
+    var rightNode   : BinaryNode?
+
+    init(_ value: String) {
+        self.value = value
+    }
+}
+
+class Queue {
+    var array = [Any]()
+
+    func values() -> [Any] {
+        var arr = [String]()
+        for index in array {
+            arr.append((index as! Node).value)
+        }
+        return arr
+    }
+    func add(_ obj: Any) {
+        array.append(obj)
+    }
+    func dequeue() -> Any? {
+        if (!array.isEmpty) {
+            return array.removeFirst()
+        }
+        return nil
+    }
+    func isEmpty() -> Bool {
+        array.isEmpty
+    }
+}
+
+var a = Node("a")
+var b = Node("b")
+var c = Node("c")
+var d = Node("d")
+var e = Node("e")
+var f = Node("f")
+var g = Node("g")
+var h = Node("h")
+var i = Node("i")
+var j = Node("j")
+
+//                 a
+//              /  |  \
+//           b     c   d
+//         /  \    |   |
+//        e    f   g   h
+//             |   |
+//             i   j
+
+func linkNodes() {
+    a.add([b, c, d])
+    b.add([e, f])
+    c.add([g])
+    d.add([h])
+    f.add([i])
+    g.add([j])
+}
+
+// Depth first search
+func depthSearch(_ root: Node, target: Node) -> Node? {
+    root.visited = true
+    if (root.value == target.value) {
+        print("found: \(root.value)")
+        return root
+    }
+
+    for node in root.nodes {
+        if (node.visited == false) {
+            print("Searching: \(node.value)")
+            if let looking = depthSearch(node, target: target) {
+                return looking
+            }
+        }
+    }
+    return nil
+}
+linkNodes()
+depthSearch(a, target: i)
+
+// Breadth first search
+func breadthSearch(_ root: Node, target: Node) -> Node? {
+    var found: Node?
+
+    let q = Queue()
+    root.visited = true
+    q.add(root)
+
+    while !q.isEmpty() {
+//        print(q.values())
+        let newRoot = q.dequeue() as! Node
+        if (newRoot.value == target.value) {
+            found = newRoot
+            break
+        }
+        for node in newRoot.nodes {
+            if (!node.visited) {
+                node.visited = true
+                q.add(node)
+            }
+        }
+    }
+    return found
+}
+//linkNodes()
+//breadthSearch(a, target: g)
+
+
+[ // a, b, c, d
+    [0, 1, 2, 0], // a
+    [1, 0, 1, 1], // b
+    [2, 1, 0, 3], // c
+    [0, 1, 3, 0]  // d
+]
+//  a - b
+//  |     \
+//    /    d
+//  |     /
+//  c - -
